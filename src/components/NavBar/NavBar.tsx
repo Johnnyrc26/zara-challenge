5f949e24d1f3d53fb6f2405b64fb84e8c86ff03f
Johnny Rojas Cifra <157323400+Johnnyrc26@users.noreply.github.com>
2025-03-15 13:35:08 +0100

    fix: correct CSS import path in NavBar component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 import { useCartStore } from '../../store/cartStore'
 import { SlBag } from 'react-icons/sl';
-import './Navbar.css'
+import './NavBar.css'
 
 interface NavbarProps {
   isLoading?: boolean 
